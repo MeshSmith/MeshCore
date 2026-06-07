yellowcooln/MeshCore-washtastic-e22p

added PCT2075 and disabled gps flags for testing

src/helpers/sensors/EnvironmentSensorManager.cpp
@@ -21,6 +21,14 @@ static Adafruit_BME680 BME680;
 static Adafruit_BMP085 BMP085;
 #endif
 
+#if ENV_INCLUDE_PCT2075
+#ifndef TELEM_PCT2075_ADDRESS
+#define TELEM_PCT2075_ADDRESS   0x37
+#endif
+#include <Adafruit_PCT2075.h>
+static Adafruit_PCT2075 PCT2075;
+#endif
+
 #if ENV_INCLUDE_AHTX0
 #define TELEM_AHTX_ADDRESS      0x38      // AHT10, AHT20 temperature and humidity sensor I2C address
 #include <Adafruit_AHTX0.h>
@@ -353,6 +361,16 @@ bool EnvironmentSensorManager::begin() {
   }
   #endif
 
+  #if ENV_INCLUDE_PCT2075
+  if (PCT2075.begin(TELEM_PCT2075_ADDRESS, TELEM_WIRE)) {
+    MESH_DEBUG_PRINTLN("Found PCT2075 at address: %02X", TELEM_PCT2075_ADDRESS);
+    PCT2075_initialized = true;
+  } else {
+    PCT2075_initialized = false;
+    MESH_DEBUG_PRINTLN("PCT2075 was not found at I2C address %02X", TELEM_PCT2075_ADDRESS);
+  }
+  #endif
+
   return true;
 }
 
@@ -502,6 +520,12 @@ bool EnvironmentSensorManager::querySensors(uint8_t requester_permissions, Cayen
         telemetry.addTemperature(TELEM_CHANNEL_SELF, BMP085.readTemperature());
         telemetry.addBarometricPressure(TELEM_CHANNEL_SELF, BMP085.readPressure() / 100);
         telemetry.addAltitude(TELEM_CHANNEL_SELF, BMP085.readAltitude(TELEM_BMP085_SEALEVELPRESSURE_HPA * 100));
+    }
+    #endif
+
+    #if ENV_INCLUDE_PCT2075
+    if (PCT2075_initialized) {
+      telemetry.addTemperature(TELEM_CHANNEL_SELF, PCT2075.getTemperature());
     }
     #endif
 

MeshSmith/MeshCore

src/helpers/sensors/EnvironmentSensorManager.cpp
@@ -52,6 +52,14 @@ static Adafruit_BME680 BME680(TELEM_WIRE);
 static Adafruit_BMP085 BMP085;
 #endif
 
+#if ENV_INCLUDE_PCT2075
+#ifndef TELEM_PCT2075_ADDRESS
+#define TELEM_PCT2075_ADDRESS 0x37
+#endif
+#include <Adafruit_PCT2075.h>
+static Adafruit_PCT2075 PCT2075;
+#endif
+
 #if ENV_INCLUDE_AHTX0
 #ifndef TELEM_AHTX_ADDRESS
 #define TELEM_AHTX_ADDRESS      0x38      // AHT10, AHT20 temperature and humidity sensor I2C address
@@ -449,6 +457,15 @@ static void query_bmp085(uint8_t ch, uint8_t, CayenneLPP& lpp) {
 }
 #endif
 
+#if ENV_INCLUDE_PCT2075
+static uint8_t init_pct2075(TwoWire* wire, uint8_t addr) {
+  return PCT2075.begin(addr, wire) ? 1 : 0;
+}
+static void query_pct2075(uint8_t ch, uint8_t, CayenneLPP& lpp) {
+  lpp.addTemperature(ch, PCT2075.getTemperature());
+}
+#endif
+
 #if ENV_INCLUDE_RAK12035
 static uint8_t init_rak12035(TwoWire* wire, uint8_t addr) {
   // RAK12035 requires setup() before begin().
@@ -600,6 +617,9 @@ static const SensorDef SENSOR_TABLE[] = {
 #endif
 #ifdef ENV_INCLUDE_BMP085
   { 0x77,                  "BMP085",       init_bmp085,   query_bmp085   },
+#endif
+#if ENV_INCLUDE_PCT2075
+  { TELEM_PCT2075_ADDRESS, "PCT2075",      init_pct2075,  query_pct2075  },
 #endif
 #if ENV_INCLUDE_RAK12035
   { TELEM_RAK12035_ADDRESS,"RAK12035",     init_rak12035, query_rak12035 },